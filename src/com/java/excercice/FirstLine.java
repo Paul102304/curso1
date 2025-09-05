ceeff79ef89a42c3abebc8c575b01357d05e3e8b
Pablo Tolteca I. <pablo.ti1023@gmail.com>
2025-09-04 18:22:14 -0600

    First commit

---
+++ src/com/java/excercice/FirstLine.java
@@ -0,0 +1,11 @@
+package com.java.excercice;
+
+public class FirstLine {
+
+	public static void main(String[] args) {
+		System.out.print("Hola mundo"); 
+		// TODO Auto-generated method stub
+
+	}
+
+}
